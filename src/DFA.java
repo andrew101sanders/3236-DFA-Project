e35bab9d9ad80911b5d8469acf13aabbd76851fb
Brad Boswell <bb05758@georgiasouthern.edu>
2018-11-18 17:04:51 -0500

    Added more exception handling to main to check file validity

--- src/DFA.java
+++ src/DFA.java
@@ -1,29 +1,35 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class DFA {
 
 	public static void main(String[] args) {
-		File file;
+		File filename;
+		File temp = new File("");
 		String in = "";
 		Scanner sc = new Scanner(System.in);
 		
 		try{
 			while(true) { //To check file name validity
-				if(args[0].contains(".txt") || in.contains(".txt")) {
-					file = new File(args[0]);
+				filename = new File(args[0]);
+				if(filename.exists() || temp.exists()) {
 					break;
 				}else {
-					System.out.println("Please enter a valid file name (with .txt):");
+					System.out.println("Please enter a valid file name:");
 					in = sc.next();
+					temp = new File(in);
 				}
 			}
 		}catch(Exception e) {
 			System.out.println("No filename was entered!\nPlease enter a valid file name (with .txt):");
 			while(true) {
 				in = sc.next();
-				if(in.contains(".txt")) {
-					file = new File(in);
+				filename = new File(in);
+				if(filename.exists()) {
 					break;
 				}else {
 					System.out.println("Please enter a valid file name (with .txt):");
@@ -32,27 +38,43 @@ public class DFA {
 		}
 		
 		boolean testing = true;
-		while(true) {
+		while(testing) {
 		
 			System.out.println("Please enter a string to test: ");
 			in = sc.next();
 			
-			//Pass 'in' to DFA logic
+//			DFA(filename, in);
 			//Print result			
 			
 			System.out.println("Would you like to check another string? Y/N");
 			in = sc.next();
+			boolean again = false;
 			if(in.contains("N")) {
-				break;
+				testing=false;
+			}else if(in.contains("Y")){
+				testing=true;
+			}else {
+				again=true;
+				while(again) {
+						System.out.println("Please type Y or N.");
+						in = sc.next();
+						if(in.contains("N")) {
+							again=false;
+							testing=false;
+							break;
+						}else if(in.contains("Y")){
+							again=false;
+							break;
+						}
+					}
 			}
 		}
 		System.out.println("Goodbye!");
-	}
-
-	public DFA(String filename) {
 		
 	}
-	
-	
+
+	private static void DFA(File file, String in) {
+
+	}
 	
 }
